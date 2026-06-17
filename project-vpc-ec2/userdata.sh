4b00bfbbf77f5fe5dc6f23effa255efe612f2eee
Charan <163161553+kattacharanvardhan@users.noreply.github.com>
2026-06-17 17:08:26 +0530

    Update userdata.sh

--- project-vpc-ec2/userdata.sh
+++ project-vpc-ec2/userdata.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>Welcome to Reyaz DnA Course</p>
+  <p>Welcome to Terraform</p>
 
 </body>
 </html>
